8f3f50c65b826e54b1c3615da5b3a95bc620e123
mohamed <esammww.com@gmail.com>
2024-07-16 02:10:16 +0300

    refactor home screen

--- lib/src/feature/home/view_model/home_view_model/home_view_model_cubit.dart
+++ lib/src/feature/home/view_model/home_view_model/home_view_model_cubit.dart
@@ -1,8 +1,16 @@
 import 'package:bloc/bloc.dart';
+import 'package:instagram_clone/src/data/firebase/app_firebase.dart';
 import 'package:meta/meta.dart';
 
 part 'home_view_model_state.dart';
 
 class HomeViewModelCubit extends Cubit<HomeViewModelState> {
   HomeViewModelCubit() : super(HomeViewModelInitial());
+  bool isLikeAnimating = false;
+  //? delete post
+  void deletePost({required String postId}) async {
+    emit(HomeViewModelLoading());
+    await AppFirebase.deletePost(postId: postId);
+    emit(HomeViewModelSuccess());
+  }
 }
